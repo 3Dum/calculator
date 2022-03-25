fd7978656bf77ef226eb8306a276b2465407a0b0
Luke <luke.goddard44@gmail.com>
2022-03-26 11:37:20 +1300

    JS ps

---
+++ main.js
@@ -0,0 +1,19 @@
+// global display var
+
+// global vars for input 1, operator and second number
+
+// add event listeners to all buttons that return the button's ID
+
+// display function to update display each time a button is pressed
+
+// input function thats called when a button is pressed
+
+// calculation functions for each operator
+
+// operate function that calculates result upon calling an operator
+// if = then resets input vars
+// else move result to input 1 and continue operation
+
+
+
+
