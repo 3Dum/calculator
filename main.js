dc5baa83ab36c7882584d9e97bd37887fa1e593b
Luke <luke.goddard44@gmail.com>
2022-03-26 11:46:26 +1300

    Set up global variables

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // global display var
+let display = document.querySelector('.screen');
+// display function to update display each time a button is pressed
+function updateDisplay(num, isResult=false) {
+  if (isResult) {
+    display.textContent = num;
+  } else {
+    display.textContent = `${display.textContent}${num}`;
+  }
+}
 
 // global vars for input 1, operator and second number
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
 
 // add event listeners to all buttons that return the button's ID
 
-// display function to update display each time a button is pressed
 
 // input function thats called when a button is pressed
 
